72c08bb00b14fcd1e18228bafa032045068e2d7d
Georgi Videv <104200628+gvidev@users.noreply.github.com>
2023-01-29 19:00:25 +0200

    adding entities and more

---
+++ CandyShop/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CandyShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ CandyShop/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace CandyShop.Entities
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
