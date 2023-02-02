5b5d4801937cf95c1ef9b0feddd0dfd457bb1486
Georgi Videv <104200628+gvidev@users.noreply.github.com>
2023-02-02 19:28:26 +0200

    Create Product.cs

---
+++ CandyShop/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Reflection.Metadata.Ecma335;
+
+namespace CandyShop.Entities
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int PieceCount { get; set; }
+        public decimal Price { get; set; }
+    }
+}
