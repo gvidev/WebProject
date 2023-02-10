fb2b448c6acbc6c1c7d4e54c209a1ae0758f1f5e
Georgi Videv <104200628+gvidev@users.noreply.github.com>
2023-02-10 17:07:49 +0200

    configuring Edit page and Index

---
+++ CandyShop/ViewModels/Account/EditVM.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CandyShop.ViewModels.Account
+{
+    public class EditVM
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+
+    }
+}
